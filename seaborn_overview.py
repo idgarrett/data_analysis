183a9c93fd0535a87cc35f50239cd3aa1b745448
Ian Garrett <ian.garrett1@gmail.com>
2017-02-13 11:38:40 -0600

    more udemy

---
+++ seaborn_overview.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 13 10:49:52 2017
+
+@author: iangarrett
+"""
+
+import seaborn as sns
+
+tips = sns.load_dataset('tips')
+
+tips.head()
+
+sns.distplot(tips['total_bill'], kde=False, bins = 30)
+
+sns.jointplot(x='total_bill', y='tip', data=tips, kind='hex')
+
+#add regression line
+sns.jointplot(x='total_bill', y='tip', data=tips, kind='regression')
+
+#compare all numerical data types
+sns.pairplot(data=tips)
+
+#add hue - categorical variable
+
+sns.pairplot(tips, hue='sex')
+
+#rugplots
+
+sns.rugplot(tips['total_bill'])
+
