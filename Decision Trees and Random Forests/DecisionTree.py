ccc1d7df66102c33dc1de3aff1595a43deeefad3
Ian Garrett <ian.garrett1@gmail.com>
2017-02-15 09:45:45 -0600

    decision trees and random forest

---
+++ Decision Trees and Random Forests/DecisionTree.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 09:38:31 2017
+
+@author: iangarrett
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+df = pd.read_csv('/Users/iangarrett/Python-Data-Science-and-Machine-Learning-Bootcamp/Machine Learning Sections/Decision-Trees-and-Random-Forests/kyphosis.csv')
+
+#exploratory Data Analysis
+
+sns.pairplot(df,hue='Kyphosis',palette='Set1')
+
+#Train Test Split
+
+from sklearn.model_selection import train_test_split
+
+X = df.drop('Kyphosis',axis=1)
+y = df['Kyphosis']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+
+from sklearn.tree import DecisionTreeClassifier
+
+dtree = DecisionTreeClassifier()
+
+dtree.fit(X_train,y_train)
+
+#Predict and Evaluate
+
+predictions = dtree.predict(X_test)
+
+from sklearn.metrics import classification_report,confusion_matrix
+
+print(classification_report(y_test,predictions))
+print('/n')
+print(confusion_matrix(y_test,predictions))
+
+from IPython.display import Image  
+from sklearn.externals.six import StringIO  
+from sklearn.tree import export_graphviz
+import pydot 
+
+features = list(df.columns[1:])
+features
+
+
+#Now let's compare the decision tree model to a random forest.
+
+from sklearn.ensemble import RandomForestClassifier
+rfc = RandomForestClassifier(n_estimators=100)
+rfc.fit(X_train, y_train)
+
+rfc_pred = rfc.predict(X_test)
+
+print(confusion_matrix(y_test,rfc_pred))
+
+print(classification_report(y_test,rfc_pred))
